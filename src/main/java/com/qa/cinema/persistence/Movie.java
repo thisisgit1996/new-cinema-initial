450fa88a3638d2f783a7a61b9f9c1ed89be9859d
thisisgit1996 <leo.baker@qa.com>
2018-01-12 11:23:13 +0000

    Added Movie Table.

---
+++ src/main/java/com/qa/cinema/persistence/Movie.java
@@ -0,0 +1,39 @@
+package com.qa.cinema.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	@Column (length = 100)
+	private String title;	
+	@Column (length = 50)
+	private String genre;
+	@Column (length = 4)
+	private String ageRating;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getAgeRating() {
+		return ageRating;
+	}
+	public void setAgeRating(String ageRating) {
+		this.ageRating = ageRating;
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Movie(id, title, genre, ageRating) values (1, 'Harry Potter', 'Adventure', '12');
+insert into Movie(id, title, genre, ageRating) values (2, 'IT', 'Horror', '18');
+insert into Movie(id, title, genre, ageRating) values (3, 'Robocop', 'Action', '15');
