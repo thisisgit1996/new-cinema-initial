18b626a7fd49042a2496ebe007aaf5309364200a
thisisgit1996 <34741707+thisisgit1996@users.noreply.github.com>
2018-01-15 16:11:55 +0000

    Revert "Added Get, Post, Put, Delete HTTP requests."

--- src/main/java/com/qa/cinema/persistence/Movie.java
+++ src/main/java/com/qa/cinema/persistence/Movie.java
@@ -9,17 +9,6 @@ import javax.persistence.Id;
 @Entity
 public class Movie {
 	
-	public Movie(Long id, String title, String genre, String ageRating) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.genre = genre;
-		this.ageRating = ageRating;
-	}
-	
-	public Movie() {
-		
-	}
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	@Column (length = 100)
@@ -47,6 +36,4 @@ public class Movie {
 	public void setAgeRating(String ageRating) {
 		this.ageRating = ageRating;
 	}
-	
-	
 }
